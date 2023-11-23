eaceadc6f170961d545fec953333b2c6748c417e
PawelKrol23 <p.krol18a@gmail.com>
2023-11-23 19:16:12 +0100

    Added some html to quiz list and added first route

--- src/app/quiz-list/quiz-list.component.ts
+++ src/app/quiz-list/quiz-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Quiz } from "../domain/quiz.model";
+import { QuizService } from "../services/quiz.service";
 
 @Component({
   selector: 'app-quiz-list',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './quiz-list.component.scss'
 })
 export class QuizListComponent {
+  quizy: Quiz[];
 
+  constructor(private quizService: QuizService) {
+    this.quizy = this.quizService.quizy;
+  }
 }
