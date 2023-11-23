ef353196d8cc89cb6b45e3b3a7623d1f67ce7309
PawelKrol23 <p.krol18a@gmail.com>
2023-11-23 23:33:09 +0100

    Added router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from "./app.component";
+import { QuizListComponent } from "./quiz-list/quiz-list.component";
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: '', redirectTo: 'quiz', pathMatch: "full" },
+  { path: 'quiz', component: QuizListComponent }
 ];
 
 @NgModule({
