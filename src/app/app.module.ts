8ab151f7a7fb0d8584c831ea1702a02b51a1fe08
PawelKrol23 <p.krol18a@gmail.com>
2023-11-24 18:26:05 +0100

    Created conductor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
 import { QuizListElementComponent } from './quiz-list/quiz-list-element/quiz-list-element.component';
+import { QuizConductorComponent } from './quiz-conductor/quiz-conductor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizListComponent,
-    QuizListElementComponent
+    QuizListElementComponent,
+    QuizConductorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quiz-conductor/quiz-conductor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-conductor',
+  templateUrl: './quiz-conductor.component.html',
+  styleUrl: './quiz-conductor.component.scss'
+})
+export class QuizConductorComponent {
+
+}
