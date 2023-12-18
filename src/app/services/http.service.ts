e85ed8dcd03948603853603576a4556277d6e725
PawelKrol23 <p.krol18a@gmail.com>
2023-12-18 17:45:12 +0100

    Ukończono serwis http

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Quiz } from "../domain/quiz.model";
-import { catchError, Observable, of } from "rxjs";
+import { map, Observable } from "rxjs";
+import { Kategoria } from "../domain/kategoria.enum";
+import { Pytanie } from "../domain/pytanie.model";
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +18,73 @@ export class HttpService {
     return this.http
       .get<Quiz[]>(this.url + "/quizzes")
       .pipe(
-        catchError(err => {
-          console.log("Error when loading quizzes: " + err);
-          return of();
+        map(body => {
+          return body.map(obj => this.mapToQuiz(obj));
         })
       );
+  }
+
+  editQuiz(quizId: number, body: Quiz): Observable<Quiz> {
+    return this.http.put(this.url + "/quizzes/" + quizId, body).pipe(
+      map(body => {
+        return this.mapToQuiz(body);
+      })
+    );
+  }
+
+  addQuiz(body: Quiz): Observable<Quiz> {
+    return this.http.post(this.url + '/quizzes', body).pipe(
+      map(body => {
+        return this.mapToQuiz(body);
+      })
+    );
+  }
+
+  deleteQuiz(id: number): Observable<Quiz> {
+    return this.http.delete(this.url + '/quizzes/' + id).pipe(
+      map(body => {
+        return this.mapToQuiz(body);
+      })
+    );
+  }
+
+  private mapToQuiz(body): Quiz {
+    let kategoria: Kategoria = null;
+    switch (body._kategoria) {
+      case "SERIALE":
+      case "Seriale":
+        kategoria = Kategoria.SERIALE;
+        break;
+      case 'FILMY':
+      case 'Filmy':
+        kategoria = Kategoria.FILMY;
+        break;
+      case 'KSIAZKI':
+      case 'Książki':
+        kategoria = Kategoria.KSIAZKI;
+        break;
+      case 'HISTORIA':
+      case 'Historia':
+        kategoria = Kategoria.HISTORIA;
+        break;
+      case 'MODA':
+      case 'Moda':
+        kategoria = Kategoria.MODA;
+        break;
+      case 'KULINARIA':
+      case 'Kulinaria':
+        kategoria = Kategoria.KULINARIA;
+        break;
+      case 'MOTORYZACJA':
+      case 'Motoryzacja':
+        kategoria = Kategoria.MOTORYZACJA;
+        break;
+    }
+
+    const pytania = body._pytania.map(pytanie => {
+      return new Pytanie(pytanie._tresc, pytanie._odpowiedzi, pytanie._poprawnaOdpowiedz);
+    });
+
+    return new Quiz(body._id, body._nazwa, kategoria, new Date(body._dataWygasniecia), pytania);
   }
 }
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,158 +1,149 @@
 import { Injectable } from '@angular/core';
 import { Quiz } from "../domain/quiz.model";
-import { Kategoria } from "../domain/kategoria.enum";
-import { Pytanie } from "../domain/pytanie.model";
 import { HttpService } from "./http.service";
 
 @Injectable({ providedIn: 'root' })
 export class QuizService {
-  quizy: Quiz[] = [
-    new Quiz(
-      1,
-      'Najlepszy quiz o serialach ever',
-      Kategoria.SERIALE,
-      new Date(2024, 0, 15, 17, 0),
-      [
-        new Pytanie(
-          'Który z poniższych seriali został stworzony przez Vince\'a Gilligana?',
-          [
-            'Stranger Things',
-            'Breaking Bad',
-            'Game of Thrones',
-            'The Crown'
-          ],
-          1
-        ),
-        new Pytanie(
-          'W którym serialu główny bohater nosił pseudonim "Heisenberg"?',
-          [
-            'Dexter',
-            'The Sopranos',
-            'Breaking Bad',
-            'The Wire'
-          ],
-          2
-        ),
-        new Pytanie(
-          'Kto jest twórcą serialu Black Mirror?',
-          [
-            'J.J. Abrams',
-            'Charlie Brooker',
-            'Ryan Murphy',
-            'Shonda Rhimes'
-          ],
-          1
-        ),
-      ]
-    ),
-    new Quiz(
-      2,
-      'Najlepszy quiz o modzie ever',
-      Kategoria.MODA,
-      new Date(2023, 11, 1, 11, 30),
-      [
-        new Pytanie(
-          'Która z tych marek słynie z czerwonej podeszwy swoich butów?',
-          [
-            'Nike',
-            'Adidas',
-            'Louboutin',
-            'Puma'
-          ],
-          2
-        ),
-        new Pytanie(
-          'Która ikona mody jest znana jako "Czarownica z Zachodu" i jest redaktorką naczelną Vogue?',
-          [
-            'Anna Wintour',
-            'Coco Chanel',
-            'Karl Lagerfeld',
-            'Giorgio Armani'
-          ],
-          0
-        ),
-        new Pytanie(
-          'Która z poniższych marek jest znana z produkcji luksusowych zegarków?',
-          [
-            'Casio',
-            'Rolex',
-            'Timex',
-            'Swatch'
-          ],
-          1
-        ),
-      ]
-    ),
-    new Quiz(
-      3,
-      'Najlepszy quiz o filmach ever',
-      Kategoria.FILMY,
-      new Date(2024, 11, 11, 11, 11),
-      [
-        new Pytanie(
-          'Który film zdobył nagrodę Oscarową za Najlepszy Film w 2020 roku?',
-          [
-            'Joker',
-            '1917',
-            'Parasite',
-            'La La Land'
-          ],
-          2
-        ),
-        new Pytanie(
-          'Który z aktorów zagrał główną rolę w filmie "The Revenant," za którą zdobył Oscara w 2016 roku?',
-          [
-            'Leonardo DiCaprio',
-            'Brad Pitt',
-            'Joaquin Phoenix',
-            'Eddie Redmayne'
-          ],
-          0
-        ),
-      ]
-    ),
-    new Quiz(
-      4,
-      'Najlepszy quiz o historii ever',
-      Kategoria.HISTORIA,
-      new Date(2024, 11, 11, 11, 11),
-      [
-        new Pytanie(
-          'Który wydarzenie rozpoczęło się 28 lipca 1914 roku i było bezpośrednią przyczyną wybuchu I wojny światowej?',
-          [
-            'Atak na Pearl Harbor',
-            'Zamach w Sarajewie',
-            'Zdobycie Berlina',
-            'Rewolucja Październikowa'
-          ],
-          1
-        ),
-        new Pytanie(
-          'Kto był liderem ruchu oporu w czasie II wojny światowej, znany z kryptonimu "Bolek"?',
-          [
-            'Winston Churchill',
-            'Charles de Gaulle',
-            'Lech Wałęsa',
-            'Jan Karski'
-          ],
-          0
-        ),
-      ]
-    ),
-  ]
-  id: number = this.quizy.length+1;
+
+  quizy: Quiz[] = [];
+
+  // quizy: Quiz[] = [
+  //   new Quiz(
+  //     1,
+  //     'Najlepszy quiz o serialach ever',
+  //     Kategoria.SERIALE,
+  //     new Date(2024, 0, 15, 17, 0),
+  //     [
+  //       new Pytanie(
+  //         'Który z poniższych seriali został stworzony przez Vince\'a Gilligana?',
+  //         [
+  //           'Stranger Things',
+  //           'Breaking Bad',
+  //           'Game of Thrones',
+  //           'The Crown'
+  //         ],
+  //         1
+  //       ),
+  //       new Pytanie(
+  //         'W którym serialu główny bohater nosił pseudonim "Heisenberg"?',
+  //         [
+  //           'Dexter',
+  //           'The Sopranos',
+  //           'Breaking Bad',
+  //           'The Wire'
+  //         ],
+  //         2
+  //       ),
+  //       new Pytanie(
+  //         'Kto jest twórcą serialu Black Mirror?',
+  //         [
+  //           'J.J. Abrams',
+  //           'Charlie Brooker',
+  //           'Ryan Murphy',
+  //           'Shonda Rhimes'
+  //         ],
+  //         1
+  //       ),
+  //     ]
+  //   ),
+  //   new Quiz(
+  //     2,
+  //     'Najlepszy quiz o modzie ever',
+  //     Kategoria.MODA,
+  //     new Date(2023, 11, 1, 11, 30),
+  //     [
+  //       new Pytanie(
+  //         'Która z tych marek słynie z czerwonej podeszwy swoich butów?',
+  //         [
+  //           'Nike',
+  //           'Adidas',
+  //           'Louboutin',
+  //           'Puma'
+  //         ],
+  //         2
+  //       ),
+  //       new Pytanie(
+  //         'Która ikona mody jest znana jako "Czarownica z Zachodu" i jest redaktorką naczelną Vogue?',
+  //         [
+  //           'Anna Wintour',
+  //           'Coco Chanel',
+  //           'Karl Lagerfeld',
+  //           'Giorgio Armani'
+  //         ],
+  //         0
+  //       ),
+  //       new Pytanie(
+  //         'Która z poniższych marek jest znana z produkcji luksusowych zegarków?',
+  //         [
+  //           'Casio',
+  //           'Rolex',
+  //           'Timex',
+  //           'Swatch'
+  //         ],
+  //         1
+  //       ),
+  //     ]
+  //   ),
+  //   new Quiz(
+  //     3,
+  //     'Najlepszy quiz o filmach ever',
+  //     Kategoria.FILMY,
+  //     new Date(2024, 11, 11, 11, 11),
+  //     [
+  //       new Pytanie(
+  //         'Który film zdobył nagrodę Oscarową za Najlepszy Film w 2020 roku?',
+  //         [
+  //           'Joker',
+  //           '1917',
+  //           'Parasite',
+  //           'La La Land'
+  //         ],
+  //         2
+  //       ),
+  //       new Pytanie(
+  //         'Który z aktorów zagrał główną rolę w filmie "The Revenant," za którą zdobył Oscara w 2016 roku?',
+  //         [
+  //           'Leonardo DiCaprio',
+  //           'Brad Pitt',
+  //           'Joaquin Phoenix',
+  //           'Eddie Redmayne'
+  //         ],
+  //         0
+  //       ),
+  //     ]
+  //   ),
+  //   new Quiz(
+  //     4,
+  //     'Najlepszy quiz o historii ever',
+  //     Kategoria.HISTORIA,
+  //     new Date(2024, 11, 11, 11, 11),
+  //     [
+  //       new Pytanie(
+  //         'Który wydarzenie rozpoczęło się 28 lipca 1914 roku i było bezpośrednią przyczyną wybuchu I wojny światowej?',
+  //         [
+  //           'Atak na Pearl Harbor',
+  //           'Zamach w Sarajewie',
+  //           'Zdobycie Berlina',
+  //           'Rewolucja Październikowa'
+  //         ],
+  //         1
+  //       ),
+  //       new Pytanie(
+  //         'Kto był liderem ruchu oporu w czasie II wojny światowej, znany z kryptonimu "Bolek"?',
+  //         [
+  //           'Winston Churchill',
+  //           'Charles de Gaulle',
+  //           'Lech Wałęsa',
+  //           'Jan Karski'
+  //         ],
+  //         0
+  //       ),
+  //     ]
+  //   ),
+  // ];
 
   constructor(private httpService: HttpService) {
-    this.httpService.getAllQuizzes().subscribe(quizy => {
-      while (this.quizy.length > 0) {
-        this.quizy.pop();
-      }
-      let zmapowaneQuizy = quizy.map(quiz => {
-        let quizObject = new Quiz(quiz.id, quiz.nazwa, Kategoria[quiz.kategoria], new Date(quiz.dataWygasniecia), quiz.pytania);
-        return quizObject;
-      });
-      this.quizy.push(...zmapowaneQuizy);
-    });
+    this.fetchQuizzes();
   }
 
   findQuizById(id: number): Quiz {
@@ -160,17 +151,39 @@ export class QuizService {
   }
 
   deleteQuizById(id: number) {
-    let index = this.quizy.findIndex(quiz => quiz.id === id)
+    this.httpService.deleteQuiz(id).subscribe(quizRes => {
+      const index = this.quizy.findIndex(quiz => {
+        return quiz.id === quizRes.id;
+      });
 
-    if (index !== -1) {
-      this.quizy.splice(index, 1)
-    }
+      if(index !== -1) {
+        this.quizy.splice(index, 1);
+      }
+    });
   }
 
   addQuiz(nazwa, kategoria, dataWygasniecia, pytania) {
-    this.quizy.push(new Quiz(this.id++, nazwa, kategoria, dataWygasniecia, pytania));
+    const quiz = new Quiz(0, nazwa, kategoria, dataWygasniecia, pytania);
+    this.httpService.addQuiz(quiz).subscribe(quiz => {
+      this.quizy.push(quiz);
+    });
   }
+
   editQuiz(id,nazwa, kategoria, dataWygasniecia, pytania) {
-    this.quizy[id-1] = new Quiz(id,nazwa,kategoria,dataWygasniecia,pytania);
+    const quiz = new Quiz(id,nazwa,kategoria,dataWygasniecia,pytania);
+    this.httpService.editQuiz(id, quiz).subscribe(quiz => {
+      const index = this.quizy.findIndex(el => el.id === id);
+      this.quizy[index] = quiz;
+    });
+  }
+
+  private fetchQuizzes() {
+    this.httpService.getAllQuizzes().subscribe(quizy => {
+      while (this.quizy.length > 0) {
+        this.quizy.pop();
+      }
+
+      this.quizy.push(...quizy);
+    });
   }
 }
